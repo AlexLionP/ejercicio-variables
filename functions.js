db0c805bb7a160e143fe2361ca30af679a459ac8
AlexLionP <alejandrodeleon904@gmail.com>
2024-01-09 13:20:55 -0600

    agregue el archivo functions y realicé los primeros 3 ejercicios

---
+++ functions.js
@@ -0,0 +1,32 @@
+/**Ejercicios para esta sesion
+ * 
+ * Declara una funcion para cada problema planteado
+ * 
+ * calcular el perimetro de un circulo
+ */
+function perimetroCirculo(radio){
+    return 2*piNumber*radio} 
+
+const perimetroCirculoFlecha = (radio) =>{return 2*piNumber*radio}
+/*
+ * calcular el area de un rectangulo
+*/
+function areaRectangulo(ancho, largo){
+    return ancho*largo} 
+    
+const areaRectanguloFlecha = (ancho, largo) =>{return ancho*largo}
+/*
+ * calcular el cuadrado de un numero
+*/
+function numeroAlCuadrado(numero){
+    return numero*numero} 
+    
+const numeroAlCuadradoFlecha = (numero) =>{return numero**2}
+/*
+ * calcular la conversion de grados celsius a farenheit
+ * calcular el valor del voltaje dadas la resistencia y la corriente
+ * calcular el volumen de una esfera
+ * 
+ * CONSIDERACIONES: poner en practica las mejores practicas para declarar funciones
+ * Los ejercicios se entregan tanto en funciones con nombre como funciones flecha
+ */
