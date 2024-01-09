a07d2b79a0081eb83b06c578b6ff9d876b833142
AlexLionP <alejandrodeleon904@gmail.com>
2024-01-08 18:09:11 -0600

    primer ejercicio

---
+++ variables.js
@@ -0,0 +1,92 @@
+/** Ejercicio para esta sesion
+ * 
+ * Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
+ * Pueden variar entre las palabras reservadas var, let y const
+ 
+ -number
+ */
+const HOUSE_CHAIRS = 10;
+console.log(HOUSE_CHAIRS);
+const HOUSE_BEDS = 4;
+console.log(HOUSE_BEDS);
+const HOUSE_TABLES = 2;
+console.log(HOUSE_TABLES);
+const HOUSE_SPOONS = 18;
+console.log(HOUSE_SPOONS);
+const HOUSE_FORKS = 16;
+console.log(HOUSE_FORKS);
+const HOUSE_TV = 2;
+console.log(HOUSE_TV);
+const HOUSE_SOFA = 3;
+console.log(HOUSE_SOFA);
+const HOUSE_FRIDGE = 1;
+console.log(HOUSE_FRIDGE);
+const HOUSE_DOORS = 10;
+console.log(HOUSE_DOORS);
+const HOUSE_SINKS = 2;
+console.log(HOUSE_SINKS);
+/*
+-string
+*/
+let mascotaJuan = "perro";
+console.log(mascotaJuan);
+var mascotaMaria = "gato";
+console.log(mascotaMaria);
+var mascotaJose = "hamster";
+console.log(mascotaJose);
+let mascotaAmanda = "iguana";
+console.log(mascotaAmanda);
+let mascotaRoberto = "cuyo";
+console.log(mascotaRoberto);
+var mascotaCecilia = "raton";
+console.log(mascotaCecilia);
+let mascotaAlberto = "tlacuache";
+console.log(mascotaAlberto);
+var mascotaAlexa = "gallina";
+console.log(mascotaAlexa);
+let mascotaSergio = "boa";
+console.log(mascotaSergio);
+var mascotaJennifer = "tarantula";
+console.log(mascotaJennifer);
+/*
+-boolean
+ */
+let tieneSala = true;
+console.log(tieneSala);
+let tieneCocina = true;
+console.log(tieneCocina);
+let tieneInvernadero = false;
+console.log(tieneInvernadero);
+let tieneFundicion = false;
+console.log(tieneFundicion);
+let tieneComedor = true;
+console.log(tieneComedor);
+let tieneRecamaras = true;
+console.log(tieneRecamaras);
+let tienePatio = false;
+console.log(tienePatio);
+let tieneGarage = true;
+console.log(tieneGarage);
+let tienePasillo = false;
+console.log(tienePasillo);
+let tieneJardin = true;
+console.log(tieneJardin);
+
+
+
+
+/*
+ * Declarar variables numericas y realizar las operaciones aritmeticas basicas
+ *      -suma
+ *      -resta
+ *      -multiplicacion
+ *      -division
+ * 
+ * Declarar variables que concatenen cadenas de texto
+ *      -5 mensajes diferentes en pantalla, sin limite de palabras.
+ * 
+ * Declarar 5 variables y convertirlas a otro tipo de dato
+ * 
+ *  CONSIDERACIONES: poner en practica las mejores practicas para declaracion de variables.
+ * Imprimir los resultados en pantalla con un console.log()
+ */
