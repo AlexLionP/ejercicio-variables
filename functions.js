ab46cf3caa554dd5cc819bd2bff0d66189a673b2
AlexLionP <alejandrodeleon904@gmail.com>
2024-01-09 13:47:03 -0600

    realicé el ejercicio 6

--- functions.js
+++ functions.js
@@ -38,6 +38,12 @@ function voltaje(resistencia, corriente){
 const voltajeFlecha = (resistencia, corriente) =>{return resistencia*corriente}
 /*
  * calcular el volumen de una esfera
+*/
+function volumenEsfera(radio){
+    return (4/3)*piNumber*radio**3} 
+
+const volumenEsferaFlecha = (radio) =>{return (4/3)*piNumber*radio**3}
+/*
  * 
  * CONSIDERACIONES: poner en practica las mejores practicas para declarar funciones
  * Los ejercicios se entregan tanto en funciones con nombre como funciones flecha
